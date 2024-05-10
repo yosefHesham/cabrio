7722206fe618c23787b1db2f7626737f09f4068d
yosefHesham <youssef.h@trufla.com>
2024-05-10 12:22:20 +0300

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        custom: {
+          primary: "#000000",
+          dropdown: "#181818",
+          dropDownTitle: "#EBEBF5",
+          dropdownButton: "#1C1C1E",
+          checkbox: "#F1F1F1",
+          inputField: "#F6F6F6",
+          card: "#B9B9B9",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
